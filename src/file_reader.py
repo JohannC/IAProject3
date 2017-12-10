303e155f1bdc2ff6caa52a142ef6bb51e379a375
JohannC <johann.corcuff@gmail.com>
2017-12-10 19:04:36 -0200

    Initial commit

---
+++ src/file_reader.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 17:46:59 2017
+
+@author: Lorenzo
+"""
+
+# Import the pandas package, then use the "read_csv" function to read
+# the labeled training data
+import os
+import io
+import unicodedata
+
+
+dir = os.getcwd();
+
+
+fh = open("fulldata.txt","w",encoding = "ansi") 
+id = 0
+for file in sorted(os.listdir(dir)):
+    if file.endswith(".txt") and file != "fulldata.txt":
+        with io.open(file, encoding="ansi") as f:
+            res = f.read()
+            res_decoded = unicodedata.normalize('NFC', res)
+            fh.write(res_decoded)
+        fh.write("\n")
+
+#Reading the files
